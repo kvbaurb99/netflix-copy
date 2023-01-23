80b5a70bbc40db530260b0d8e5067f34cbc2f453
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-23 22:52:32 +0100

    buttons fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ export default function NavBar() {
     <div className='w-full flex justify-between md:p-7 absolute z-20 p-4'>
         <h1 className='logo text-red-600 text-5xl md:text-6xl tracking-wider font-bold ml-[2rem] cursor-pointer'>NETFLIX</h1>
         <div className='mr-[2rem]'>
-            <button className='bg-white px-1 md:px-9 md:py-2 text-black mr-[1rem] md:mr-[2rem] rounded hover:bg-gray-200'>Log In</button>
-            <button className='bg-red-600 px-1 md:px-9 md:py-2 rounded hover:bg-red-700'>Sign Up</button>
+            <button className='bg-white px-1 md:px-10 md:py-2 text-black mr-[1rem] md:mr-[2rem] rounded hover:bg-gray-200'>Log In</button>
+            <button className='bg-red-600 px-1 md:px-12 md:py-2 rounded hover:bg-red-700'>Sign Up</button>
         </div>
     </div>
   )
