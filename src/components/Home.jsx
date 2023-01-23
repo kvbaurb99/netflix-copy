fba355c240974613f69778835905e24f6d93c3a0
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-23 20:29:15 +0100

    home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,29 @@ import React, { useEffect, useState } from 'react'
 import getMovies from '../api/apiRequest';
 import axios from 'axios';
 import NavBar from './NavBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home({randomMovie}) {
 
   return (
-    <div className='w-full h-full'>
-        <img src={`https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}`} alt="" />
+    <div className='w-full h-[800px]'>
+        <div className='w-full h-full'>
+            <div className='absolute w-full h-[800px] bg-gradient-to-r from-black'></div>
+            <img src={`https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}`} alt={randomMovie?.title} className='w-full h-full object-cover' />
+            <div className='absolute top-[30%] w-full px-[6rem]'>
+                <div className='w-[800px]'>
+                    <p className='text-6xl font-bold'>{randomMovie?.title}</p>
+                    <p className='text-xl font-bold mt-[1rem]'>Available Now</p>
+                    <p className='text-lg mt-[1rem] w-full md:max-w-[70%] lg:max-w-[60%]'>{randomMovie?.overview}</p>
+                </div>
+                <div className='mt-[2rem]'>
+                    <button className='bg-white text-black px-12 py-3 rounded border-black border-solid border text-lg font-bold'><FontAwesomeIcon icon={faPlay}/> Play</button>
+                    <button className='bg-gray-700 px-14 py-3 ml-3 rounded text-lg font-bold'>More Info</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className='w-full flex justify-between p-7 absolute'>
+    <div className='w-full flex justify-between p-7 absolute z-20'>
         <h1 className='logo text-red-600 text-6xl tracking-wider font-bold ml-[2rem] cursor-pointer'>NETFLIX</h1>
         <div className='mr-[2rem]'>
             <button className='bg-white px-9 py-2 text-black mr-[2rem] rounded hover:bg-gray-200'>Log In</button>
