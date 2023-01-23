92fde2989a5ab6d9a3e0d84f91401f1d490a5590
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-23 22:52:26 +0100

    sliding function

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,10 +5,11 @@ import {FaHeart, FaRegHeart} from 'react-icons/fa'
 import { faArrowRight, faArrowLeft  } from '@fortawesome/free-solid-svg-icons'
 import Movie from './Movie'
 
-export default function Slider({fetchURL, title}) {
+export default function Slider({fetchURL, title, sliderId}) {
 
     const [movies, setMovies] = useState([])
-  
+
+     
 
     useEffect(() => {
         axios.get(fetchURL).then((res) => {
@@ -16,19 +17,31 @@ export default function Slider({fetchURL, title}) {
         })
     }, [fetchURL])
 
-    console.log(movies)
+    const slideLeft = () => {
+      let imageSlider = document.getElementById(`imageslider ${sliderId}`);
+      imageSlider.scrollLeft -= 500;
+    }
+
+    const slideRight = () => {
+      let imageSlider = document.getElementById(`imageslider ${sliderId}`);
+      imageSlider.scrollLeft += 500;
+    }
+
+
+
+
 
   return (
-    <div>
-        <h2 className='font-bold md:text-xl p-5'>{title}</h2>
-        <div className='flex items-center relative'>
-            <FontAwesomeIcon icon={faArrowLeft} size='xl' className='bg-white rounded-full opacity-70 text-black p-2 absolute left-0 z-20 hover:opacity-100 cursor-pointer' />
-           <div className='w-full h-full overflow-x-scroll scroll-smooth whitespace-nowrap relative scrollbar-hide'>
+    <div className='mt-[1.5rem]'>
+        <h2 className='font-bold md:text-2xl p-5 tracking-wide'>{title}</h2>
+        <div className='flex items-center relative' >
+            <FontAwesomeIcon onClick={slideLeft} icon={faArrowLeft} size='xl' className='bg-white rounded-full opacity-50 text-black p-2 absolute left-0 z-20 hover:opacity-100 cursor-pointer' />
+           <div id={`imageslider ${sliderId}`} className='w-full h-full overflow-x-scroll scroll-smooth whitespace-nowrap relative  scrollbar-hide'>
               {movies.map((movie, id) => {
                 return <Movie movie={movie} id={id} />
               })}  
            </div>
-           <FontAwesomeIcon icon={faArrowRight} size='xl' className='bg-white rounded-full opacity-70 text-black p-2 absolute right-0 z-20 hover:opacity-100 cursor-pointer' />
+           <FontAwesomeIcon onClick={slideRight} icon={faArrowRight} size='xl' className='bg-white rounded-full opacity-50 text-black p-2 absolute right-0 z-20 hover:opacity-100 cursor-pointer' />
         </div>
     </div>
   )
