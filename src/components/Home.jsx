e93403dbb7c7cd0e9e5d22edc6a9cc6d6222a4f4
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-25 11:29:54 +0100

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import Footer from './Footer';
+
 
 
 export default function Home({randomMovie}) {
