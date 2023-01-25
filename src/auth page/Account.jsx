4a032bbc814f004567b9db72065efdee12831b0b
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-01-25 11:28:11 +0100

    fix

--- src/auth page/Account.jsx
+++ src/auth page/Account.jsx
@@ -8,7 +8,7 @@ export default function Account() {
         <NavBar />
         <img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/netflixteaser.png" alt="" className='object-cover h-[500px] w-full' />
         <div className='fixed top-0 left-0 w-full bg-black/60 h-[500px]'></div>
-        <div className='absolute top-[25%] p-8 text-3xl font-bold tracking-wide'>
+        <div className='absolute top-[25%] p-8 text-5xl font-bold tracking-wide'>
           <h1>Favorite Movies</h1>
         </div>
         <SavedMovies />
